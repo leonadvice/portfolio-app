1469cee67a1cc5b1915385c1ddbf03ba539fb47d
Nhat-Nguyen-011 <nhat.nguyen1125@gmail.com>
2020-05-28 14:34:33 +0700

    add email feature

---
+++ public/js/main.js
@@ -0,0 +1,40 @@
+const chatForm = document.getElementById('chatForm');
+
+chatForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  (async () => {
+    const request = await fetch('./post', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email: e.target.elements.email.value,
+        name: e.target.elements.name.value,
+        message: e.target.elements.message.value,
+      }),
+    });
+    const response = await request.json();
+
+    const resDiv = document.getElementById('response');
+    resDiv.innerHTML = '';
+    let newRes = document.createElement('div');
+
+    if (response.status == 'ok') {
+      newRes.classList.add('ok');
+      newRes.textContent =
+        'Message sent successfully. Please check your email for comfirmation.';
+
+      document.getElementById('name').value = '';
+      document.getElementById('email').value = '';
+      document.getElementById('message').value = '';
+    } else {
+      newRes.classList.add('err');
+      newRes.textContent = response.status;
+    }
+
+    resDiv.appendChild(newRes);
+  })();
+});
